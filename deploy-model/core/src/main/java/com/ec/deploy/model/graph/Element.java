bc563f160c4c0b18f8285721973f7dc43852246b
jhaswell <jhaswell@electric-cloud.com>
2012-11-26 13:16:40 -0700

    Added Element as a mapped superclass.

--- deploy-model/core/src/main/java/com/ec/deploy/model/graph/Element.java
+++ deploy-model/core/src/main/java/com/ec/deploy/model/graph/Element.java
@@ -6,6 +6,18 @@ public abstract class Element<E extends Element<E>> extends PersistentEntity<E>
 {
     private Graph container;
 
+    public Element(final Graph container) {
+        this.container = container;
+    }
+
+    public Graph getContainer() {
+        return container;
+    }
+
+    public void setContainer(Graph container) {
+        assert container != null : "Container must not be null!";
+        this.container = container;
+    }
     @Override
     public abstract E clone();
 }
